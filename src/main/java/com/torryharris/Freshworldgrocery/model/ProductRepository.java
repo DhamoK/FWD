6ff85cdc7fd2d5bcd694a59024dc346d7bcac6cd
DhamoK <89825799+DhamoK@users.noreply.github.com>
2022-01-10 10:31:58 +0530

    Update ProductRepository.java

--- src/main/java/com/torryharris/Freshworldgrocery/model/ProductRepository.java
+++ src/main/java/com/torryharris/Freshworldgrocery/model/ProductRepository.java
@@ -3,10 +3,11 @@ package com.torryharris.Freshworldgrocery.model;
 import java.util.List;
 
 import com.torryharris.Freshworldgrocery.model.Products;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ProductRepository extends CrudRepository<Products, Integer> {
+public interface ProductRepository extends JpaRepository<Products, Integer> {
 }
